c810ea22f91c63117f64758f60635ce29388dd4b
Noam Tal <nt1825@imperial.ac.uk>
2026-02-06 20:48:28 +0000

    test

--- backend/app/routers/items.py
+++ backend/app/routers/items.py
@@ -16,7 +16,7 @@ fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
 @router.get("/")
 async def read_items():
     return fake_items_db
-
+ 
 
 @router.get("/{item_id}")
 async def read_item(item_id: str):
